c5e736e42076ec78715cb978fa615d0df9908bf3
gitoff-start <replyban@gmail.com>
2023-07-27 14:23:26 +0500

    'wcf service client access'

---
+++ wcfClient/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+using wcfClient.ServiceReference1;
+
+
+
+namespace wcfClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Service1Client service1Client = new Service1Client();
+
+
+            List<wcfClient.ServiceReference1.Country> list =  service1Client.GetCountries().ToList();
+            
+             
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
