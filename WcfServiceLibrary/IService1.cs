369e7e7915c9cef9812408505a337426c94927ac
gitoff-start <replyban@gmail.com>
2023-07-27 10:09:24 +0500

    Add project files.

---
+++ WcfServiceLibrary/IService1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceLibrary
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        string GetData(string value);
+
+        [OperationContract]
+        int GetTotalMarks(Student s); //passing object to acces subjects 
+
+
+        [OperationContract]
+        int getAvgMarks(int s1, int s2, int s3);
+
+        // TODO: Add your service operations here
+    }
+
+    [DataContract] //DataContract is to serialze class to use it over network
+   public class Student
+    {
+        [DataMember]  // it serialize class member to use a serialize 
+        public int s1 { get; set; }
+        [DataMember]
+        public int s2 { get; set; }
+        [DataMember]
+        public int s3 { get; set; }
+    }
+
+}
---
+++ WcfServiceLibrary/Service1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+
+namespace WcfServiceLibrary
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Service1 : IService1
+    {
+    
+
+        public string GetData(string value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+      
+
+
+        public int getAvgMarks(int s1, int s2, int s3)
+        {
+            return s1 + s2+s3;
+        }
+
+        int IService1.GetTotalMarks(Student s)
+        {
+            return s.s1 + s.s2+s.s3; 
+        }
+    }
+}
